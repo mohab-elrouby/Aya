0d4c216fe71ace38d88e7b78432c46ff45caf75f
Mohab Mohammad <mohab.mohammad3@gmail.com>
2023-03-31 06:39:04 +0200

    First Commit

---
+++ Aya/AyaPackage.cs
@@ -0,0 +1,244 @@
+﻿global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using System;
+global using Task = System.Threading.Tasks.Task;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Microsoft.VisualStudio;
+using StreamJsonRpc;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Notifications;
+
+namespace Aya
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("Aya", Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.AyaString)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionOpening_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "Aya", "General", 0, 0, true, SupportsProfiles = true)]
+    public sealed class AyaPackage : ToolkitPackage
+    {
+        public static int _interval;
+        private static System.Timers.Timer _timer;
+        private static string ayaNum, sorahNum;
+        public static Dictionary<int, string> quranSorahs = new Dictionary<int, string>
+{
+    {1, "الفاتحة"},
+    {2, "البقرة"},
+    {3, "آل عمران"},
+    {4, "النساء"},
+    {5, "المائدة"},
+    {6, "الأنعام"},
+    {7, "الأعراف"},
+    {8, "الأنفال"},
+    {9, "التوبة"},
+    {10, "يونس"},
+    {11, "هود"},
+    {12, "يوسف"},
+    {13, "الرعد"},
+    {14, "إبراهيم"},
+    {15, "الحجر"},
+    {16, "النحل"},
+    {17, "الإسراء"},
+    {18, "الكهف"},
+    {19, "مريم"},
+    {20, "طه"},
+    {21, "الأنبياء"},
+    {22, "الحج"},
+    {23, "المؤمنون"},
+    {24, "النور"},
+    {25, "الفرقان"},
+    {26, "الشعراء"},
+    {27, "النمل"},
+    {28, "القصص"},
+    {29, "العنكبوت"},
+    {30, "الروم"},
+    {31, "لقمان"},
+    {32, "السجدة"},
+    {33, "الأحزاب"},
+    {34, "سبأ"},
+    {35, "فاطر"},
+    {36, "يس"},
+    {37, "الصافات"},
+    {38, "ص"},
+    {39, "الزمر"},
+    {40, "غافر"},
+    {41, "فصلت"},
+    {42, "الشورى"},
+    {43, "الزخرف"},
+    {44, "الدخان"},
+    {45, "الجاثية"},
+    {46, "الأحقاف"},
+    {47, "محمد"},
+    {48, "الفتح"},
+    {49, "الحجرات"},
+    {50, "ق"},
+    {51, "الذاريات"},
+    {52, "الطور"},
+    {53, "النجم"},
+    {54, "القمر"},
+    {55, "الرحمن"},
+    {56, "الواقعة"},
+    {57, "الحديد"},
+    {58, "المجادلة"},
+    {59, "الحشر"},
+    {60, "الممتحنة"},
+    {61, "الصف"},
+    {62, "الجمعة"},
+    {63, "المنافقون"},
+    {64, "التغابن"},
+    {65, "الطلاق"},
+    {66, "التحريم"},
+    {67, "الملك"},
+    {68, "القلم"},
+    {69, "الحاقة"},
+    {70, "المعارج"},
+    {71, "نوح"},
+    {72, "الجن"},
+    {73, "المزمل"},
+    {74, "المدثر"},
+    {75, "القيامة"},
+    {76, "الانسان"},
+    {77, "المرسلات"},
+    {78, "النبأ"},
+    {79, "النازعات"},
+    {80, "عبس"},
+    {81, "التكوير"},
+    {82, "الانفطار"},
+    {83, "المطففين"},
+    {84, "الانشقاق"},
+    {85, "البروج"},
+    {86, "الطارق"},
+    {87, "الأعلى"},
+    {88, "الغاشية"},
+    {89, "الفجر"},
+    {90, "البلد"},
+    {91, "الشمس"},
+    {92, "الليل"},
+    {93, "الضحى"},
+    {94, "الشرح"},
+    {95, "التين"},
+    {96, "العلق"},
+    {97, "القدر"},
+    {98, "البينة"},
+    {99, "الزلزلة"},
+    {100, "العاديات"},
+    {101, "القارعة"},
+    {102, "التكاثر"},
+    {103, "العصر"},
+    {104, "الهمزة"},
+    {105, "الفيل"},
+    {106, "قريش"},
+    {107, "الماعون"},
+    {108, "الكوثر"},
+    {109, "الكافرون"},
+    {110, "النصر"},
+    {111, "المسد"},
+    {112, "الإخلاص"},
+    {113, "الفلق"},
+    {114, "الناس"}
+};
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            General.Saved += OnOptionsSaved;
+            _interval = GetDuration();
+            SetTimer();
+        }
+        public static async void ShowNotification(Object o, EventArgs e)
+        {
+            var str = "";
+            var path1 = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var path = Path.GetFullPath(Path.Combine(path1, @"..\"))+"Resources\\logo-no-background.png";
+            try
+            {
+                str = await ReadAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            sorahNum = str.Split('|')[0];
+            ayaNum = str.Split('|')[1];
+            new ToastContentBuilder()
+            .SetToastScenario(ToastScenario.Reminder)
+            .AddText(quranSorahs[int.Parse(str.Split('|')[0])]+" : آية "+ayaNum)
+            .AddText(str.Split('|')[2])
+            .AddAppLogoOverride(new Uri(path))
+            .AddButton(new ToastButton().SetContent("تفسير الآية").AddArgument(key: "action", "tafseer"))
+            .AddButton(new ToastButton().SetContent("أغلق").AddArgument("action", "dismiss"))
+            .AddAudio(null, null, true)
+            .Show(t => {
+                t.ExpirationTime = DateTime.Now.AddSeconds(10);
+                t.Activated += HandleToastActivation;
+            });
+        }
+        public static async Task<string> ReadAsync()
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var path1 = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var path = Path.GetFullPath(Path.Combine(path1, @"..\"))+"Resources\\quran.txt";
+            StreamReader sr = new StreamReader(path);
+            Random rnd = new Random();
+            int random = rnd.Next(6235);
+            for (int i = 0; i < random; i++)
+            {
+                await sr.ReadLineAsync();
+            }
+            return await sr.ReadLineAsync();
+        }
+        public static async Task<General> GetOptionsAsync()
+        {
+            General option = await General.GetLiveInstanceAsync();
+            return option;
+        }
+        public static int GetDuration()
+        {
+            var option = GetOptionsAsync().Result;
+            var duration = option.duration;
+            return duration;
+        }
+        private void OnOptionsSaved(General e)
+        {
+            var option = GetOptionsAsync().Result;
+            _timer.Interval = GetDuration();
+        }
+        public static void SetTimer()
+        {
+            _timer = new System.Timers.Timer(_interval);
+            _timer.Elapsed += ShowNotification;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+        }
+        private static void HandleToastActivation(ToastNotification sender, object args)
+        {
+            ToastActivatedEventArgs strArgs = args as ToastActivatedEventArgs;
+            var argsValues = strArgs.Arguments;
+            switch (argsValues.Split('=')[1])
+            {
+                case "tafseer":
+                    Process proc = new Process();
+                    proc.StartInfo.UseShellExecute = true;
+                    proc.StartInfo.FileName = $"http://quran.ksu.edu.sa/tafseer/katheer/sura{sorahNum}-aya{ayaNum}.html";
+                    proc.Start();
+                    //stuff
+                    break;
+                case "dismiss":
+                    //stuff
+                    break;
+                case "bodyTapped":
+                    //stuff
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Aya/Options/General.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace Aya
+{
+    internal partial class OptionsProvider
+    {
+        // Register the options with this attribute on your package class:
+        // [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "Aya", "General", 0, 0, true, SupportsProfiles = true)]
+        [ComVisible(true)]
+        public class GeneralOptions : BaseOptionPage<General> { }
+    }
+
+    public class General : BaseOptionModel<General>
+    {
+        [Category("Aya")]
+        [DisplayName("Duration")]
+        [Description("Set duration for how often you want to see an Aya")]
+        [DefaultValue(100000)]
+        public int duration { get; set; }
+        
+    }
+}
